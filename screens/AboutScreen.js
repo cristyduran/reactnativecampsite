da40c155a51f8b84b267e2ca52b4a1e66d247e5f
Cristy Duran <duran_cris43@yahoo.com>
2023-06-10 11:49:01 -0700

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { ScrollView, Text } from "react-native";
 import { Card, Avatar, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from "../shared/baseUrl";
+import Loading from '../components/LoadingComponent';
 
 function Mission() {
     return (
@@ -17,6 +18,31 @@ function Mission() {
 
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider/>
+                <Loading />
+            </Card>
+        </ScrollView>
+        )
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider/>
+                <Text>{partners.errMess}</Text>
+            </Card>
+        </ScrollView>
+        )
+    }
 
     return (
         <ScrollView>
