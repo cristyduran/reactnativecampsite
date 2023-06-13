260c6f18060b2953b6c94ced83ed79c086273785
Cristy Duran <duran_cris43@yahoo.com>
2023-06-12 18:27:34 -0700

    Modal

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { 
-    Text, 
-    View, 
-    ScrollView, 
-    StyleSheet, 
-    Switch, 
-    Button 
+import {
+    Text,
+    View,
+    ScrollView,
+    StyleSheet,
+    Switch,
+    Button,
+    Modal
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -15,7 +16,8 @@ const ReservationScreen = () => {
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
-    
+    const [showModal, setShowModal] = useState(false);
+
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         setShowCalendar(Platform.OS === 'ios');
@@ -26,12 +28,15 @@ const ReservationScreen = () => {
         console.log('campers:', campers);
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
+        setShowModal(!showModal);
+    };
+
+    const resetForm = () => {
         setCampers(1);
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
-
-    }
+    };
 
     return (
         <ScrollView>
@@ -42,12 +47,12 @@ const ReservationScreen = () => {
                     selectedValue={campers}
                     onValueChange={(itemValue) => setCampers(itemValue)}
                 >
-                    <Picker.Item label='1' value={1}/>
-                    <Picker.Item label='2' value={2}/>
-                    <Picker.Item label='3' value={3}/>
-                    <Picker.Item label='4' value={4}/>
-                    <Picker.Item label='5' value={5}/>
-                    <Picker.Item label='6' value={6}/>
+                    <Picker.Item label='1' value={1} />
+                    <Picker.Item label='2' value={2} />
+                    <Picker.Item label='3' value={3} />
+                    <Picker.Item label='4' value={4} />
+                    <Picker.Item label='5' value={5} />
+                    <Picker.Item label='6' value={6} />
                 </Picker>
             </View>
             <View style={styles.formRow}>
@@ -80,14 +85,43 @@ const ReservationScreen = () => {
             <View style={styles.formRow}>
                 <Button
                     onPress={() => handleReservation()}
-                    title='Search Availablility'
+                    title='Search Availability'
                     color='#5637DD'
                     accessibilityLabel='Tap me to search for available campsites to reserve'
                 />
             </View>
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>
+                        Search Campsite Reservations
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Number of Campers: {campers}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Hike-In?: {hikeIn ? 'Yes' : 'No'}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Date: {date.toLocaleDateString('en-US')}
+                    </Text>
+                    <Button
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                        color='#5637DD'
+                        title='Close'
+                    />
+                </View>
+            </Modal>
         </ScrollView>
     );
-;}
+};
 
 const styles = StyleSheet.create({
     formRow: {
@@ -103,6 +137,22 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#5637DD',
+        textAlign: 'center',
+        color: '#fff',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
     }
 });
 
